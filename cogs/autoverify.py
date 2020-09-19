646aa2a7f11746b8e397a7635dd2589e6cfc2060
Kroko <schrottsuperdeluxe@gmail.com>
2020-09-19 21:55:16 +0200

    Added Autoverify and Text Utility

---
+++ cogs/autoverify.py
@@ -0,0 +1,67 @@
+import json
+import discord
+from discord.ext import commands
+
+
+class Autoverify(commands.Cog):
+
+	def __init__(self, client):
+		self.client = client
+		with open('./data/roles/mainrole.json', 'r') as m:
+			self.mainroles = json.load(m)
+		with open('./data/roles/spacer.json', 'r') as s:
+			self.spacers = json.load(s)
+	
+	@commands.command()
+	@commands.has_permissions(administrator=True)
+	async def mainrole(self, ctx, role: discord.Role):
+		"""Setze die Rolle die neue User bekommen"""
+		if role.guild is ctx.guild:
+			self.mainroles[str(ctx.guild.id)] = int(role.id)
+			with open('./data/roles/mainrole.json', 'w') as f:
+				json.dump(self.mainroles, f, indent=4)
+			await ctx.send("Main-Rolle gesetzt.", delete_after=self.client.del_time_mid)
+		else:
+			await ctx.send("Rolle wurde auf dem Server nicht gefunden.", delete_after=self.client.del_time_mid)
+	
+	@commands.command()
+	@commands.has_permissions(administrator=True)
+	async def spacer(self, ctx, mode, role: discord.Role):
+		"""Füge eine Spacerrolle hinzu oder entferne diese"""
+		if role.guild.id != ctx.guild.id and (mode == "add" or mode == "remove"):
+			print(role.guild)
+			print(ctx.guild)
+			await ctx.send("Rolle wurde auf dem Server nicht gefunden.", delete_after=self.client.del_time_mid)
+		elif mode == "add":
+			if str(ctx.guild.id) not in self.spacers.keys():
+				self.spacers[str(ctx.guild.id)] = []
+			self.spacers[str(ctx.guild.id)].append(role.id)
+			with open('./data/roles/spacer.json', 'w') as f:
+				json.dump(self.spacers, f, indent=4)
+			await ctx.send(f'Rolle {role} hinzugefügt.', delete_after=self.client.del_time_mid)
+		elif mode == "remove":
+			self.spacers[str(ctx.guild.id)].remove(role.id)
+			with open('./data/roles/spacer.json', 'w') as f:
+				json.dump(self.spacers, f, indent=4)
+			await ctx.send("Spacerrolle entfernt.", delete_after=self.client.del_time_mid)
+		else:
+			await ctx.send("Ungültiger Mode! Verfügbare Modi:\nadd\tremove")
+		
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		if message.guild is not None and message.author is not None and len(message.author.roles) == 1:
+			try:
+				member = message.guild.get_member(int(message.author.id))
+				# Main Rolle setzen
+				role = message.guild.get_role(self.mainroles[str(message.guild.id)])
+				await member.add_roles(role, reason="verify (mainrole)")
+				# Spacer setzen
+				for rid in self.spacers[str(message.guild.id)]:
+					trole = message.guild.get_role(rid)
+					await member.add_roles(trole, reason="verify (spacer)")
+			except KeyError:
+				pass
+
+
+def setup(client):
+	client.add_cog(Autoverify(client))
---
+++ cogs/textutility.py
@@ -0,0 +1,151 @@
+import discord
+import datetime as dt
+from discord.ext import commands, tasks
+
+
+# 60 sekunden 60 minuten 24 stunden 3 tage
+ARCHIV_TIME = 60 * 60 * 24 * 3
+
+
+class TextUtility(commands.Cog):
+	
+	def __init__(self, client):
+		self.client = client
+		self.topiccreate = dict()
+		self.categorys = dict()
+		self.archives = dict()
+	
+	def cog_unload(self):
+		self.themen_garbage_collector.cancel()
+	
+	@tasks.loop(seconds=10)
+	async def themen_garbage_collector(self):
+		now = dt.datetime.utcnow()
+		
+		def initalise(guild_id):
+			local = self.client.get_guild(guild_id)
+			return local, local.get_channel(self.archives[guild_id]), local.get_channel(self.categorys[guild_id])
+		
+		async def clean_archive(archive):
+			if len(archive.voice_channels) > 0:
+				for voice_channel in archive.voice_channels:
+					await voice_channel.delete()
+			elif len(archive.channels) >= 20:
+				del_channel, time = 0, now
+				for ch in archive.text_channels:
+					msg = await ch.history(limit=1).next()
+					if time > msg.created_at:
+						del_channel, time = ch, msg.created_at
+				await del_channel.delete()
+		
+		for guildid in self.archives.keys():
+			try:
+				guild, archivcategory, category = initalise(guildid)
+			except Exception:
+				continue
+			
+			for channel in category.text_channels:
+				if guildid in self.topiccreate and channel.id == self.topiccreate[guildid]:
+					continue
+				try:
+					message = await channel.history(limit=1).next()
+				except discord.errors.NoMoreItems:
+					await channel.delete()
+				else:
+					if round((now - message.created_at).total_seconds()) > ARCHIV_TIME:
+						await clean_archive(archive=archivcategory)
+						await channel.edit(reason="Archivieren", category=archivcategory)
+	
+	@commands.Cog.listener()
+	async def on_ready(self):
+		for g in self.client.guilds:
+			try:
+				self.topiccreate[g.id] = int(self.client.dbconf_get(g.id, "create"))
+				self.categorys[g.id] = int(self.client.dbconf_get(g.id, "category"))
+				self.archives[g.id] = int(self.client.dbconf_get(g.id, "archiv"))
+			except TypeError:
+				pass
+			except IndexError:
+				pass
+		self.themen_garbage_collector.start()
+	
+	# Command to set Values in the Server Config
+	@commands.command()
+	@commands.has_permissions(administrator=True)
+	async def textchannel(self, ctx, key, value=""):
+		"""Setzt einen textchannel <key>, <value> nutze den Key help für eine übsericht aller Keys"""
+		
+		async def setchannel():
+			if not value.isnumeric():
+				raise ValueError
+			self.client.dbconf_set(ctx.guild.id, key, value)
+		
+		if key == "create":
+			await setchannel()
+			await ctx.send(f"{key.capitalize()}-Channel erfolgreich gesetzt!", delete_after=self.client.del_time_small)
+			self.topiccreate[ctx.guild.id] = int(self.client.dbconf_get(ctx.guild.id, "create"))
+		elif key == "category":
+			await setchannel()
+			await ctx.send(f"{key.capitalize()}-Channel erfolgreich gesetzt!", delete_after=self.client.del_time_small)
+			self.categorys[ctx.guild.id] = int(self.client.dbconf_get(ctx.guild.id, "category"))
+		elif key == "archiv":
+			await setchannel()
+			await ctx.send(f"{key.capitalize()}-Channel erfolgreich gesetzt!", delete_after=self.client.del_time_small)
+			self.archives[ctx.guild.id] = int(self.client.dbconf_get(ctx.guild.id, "archiv"))
+		else:
+			await ctx.send(
+				embed=discord.Embed(
+					title="Textchannel-Help",
+					description=
+					"create\tKanal zum erstellen von Kanälen\ncategory\tSetzt die Kategorie in welcher Kanäle erstellt"
+					" werden sollen\nauto-react\tSetzt einen Kanal für automatische Reaktionen"))
+	
+	# wenn message im Topic Channel
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		if message.guild is None or message.author.id == self.client.user.id or message.guild.id not in self.topiccreate:
+			return
+		
+		elif message.channel.category_id == self.archives[message.guild.id]:
+			category = message.guild.get_channel(self.categorys[message.guild.id])
+			if len(category.channels) < 20:
+				await message.channel.edit(category=category)
+		elif message.channel.id == self.topiccreate[message.guild.id]:
+			await message.delete()
+			if message.author.bot:
+				await message.channel.send(
+					"Nachrichten anderer Bots sind hier nichte erlaubt bitte führe hier keine Befehle aus!",
+					delete_after=self.client.del_time_mid)
+			elif len(str(message.content)) > 100:
+				await message.channel.send(
+					"Zu Langer Titel! Dein Titel beim Thema erstellen ist zu lang! (>100 Zeichen)",
+					delete_after=self.client.del_time_mid)
+			else:
+				
+				try:
+					# neuer channel erstellen
+					category = message.guild.get_channel(self.categorys[message.guild.id])
+					if category is None:
+						raise KeyError
+					new = await category.create_text_channel(
+						message.content, position=0, topic=message.content,
+						reason=f"Neuer Channel von User {str(message.author)} zum Thema {message.content}")
+					await message.channel.send(
+						f"Channel {message.content} erfolgreich erstellt.", delete_after=self.client.del_time_small)
+					await new.send(embed=discord.Embed(
+						title=f"Neues Thema {message.content}",
+						description=f"Das neue Thema {message.content} wurde von User <@{message.author.id}> erstellt."))
+				except KeyError:  # Konnte Kategorie nicht finden
+					await message.channel.send(
+						f"Konnte keine Kategorie für ein neues Thema finden. Bitte kontaktiere einen Admin und "
+						f"bitte ihn eine Kategorie mit {self.client.prefixes[message.guild.id]}textchannel kategorie "
+						f"zu setzen. Oder diesen Kanal als Themen-Erstellungskanal zu entfernen.",
+						delete_after=self.client.del_time_long)
+				except discord.DiscordException:
+					await message.send(
+						"Konnte den Kanal nicht erstellen. Bitte überprüfe deinen Kanalnamen.",
+						delete_after=self.client.del_time_mid)
+
+
+def setup(client):
+	client.add_cog(TextUtility(client))
