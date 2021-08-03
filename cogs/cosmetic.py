41cfe7c7e2f37c6e523d18dc4f51d96a1b165fbe
Kroko-fant <schrottsuperdeluxe@gmail.com>
2021-08-03 16:39:17 +0200

    Updated Dawum to Version 2.0.0

--- cogs/cosmetic.py
+++ cogs/cosmetic.py
@@ -9,7 +9,7 @@ class Cosmetic(commands.Cog):
 
 	def __init__(self, client):
 		self.client = client
-		self.version = "1.5.0"
+		self.version = "2.0.0"
 
 	@commands.Cog.listener()
 	async def on_command(self, ctx):
--- cogs/dawum.py
+++ cogs/dawum.py
@@ -9,12 +9,13 @@ from discord.ext import commands
 # Notice! You are not allowed to use this module without giving the credits to https://dawum.de/api/
 
 parlamentcodes = {
-    'bt': 0, 'bundestag': 0, 'bw': 1, 'bawü': 1, 'de-bw': 1, 'baden': 1, 'badenwürttemberg': 1, 'by': 2,
+    'bt': 0, 'bundestag': 0, 'bw': 1, 'bawü': 1, 'de-bw': 1, 'baden': 1, 'baden-württemberg': 1, 'by': 2,
     'bay': 2, 'de-by': 2, 'bayern': 2, 'be': 3, 'de-be': 3, 'berlin': 3, 'ber': 3, 'bb': 4, 'de-bb': 4,
     'brandenburg': 4, 'brand': 4, 'hb': 5, 'de-hb': 5, 'bremen': 5, 'hh': 6, 'de-hh': 6, 'hamburg': 6, 'ham': 6,
     'he': 7, 'de-he': 7, 'hessen': 7, 'mv': 8, 'de-mv': 8, 'mecklenburg': 8, 'vorpommern': 8,
     'mecklenburg-vorpommern': 8, 'ni': 9, 'de-ni': 9, 'nie': 9, 'nieder': 9, 'niedersachsen': 9, 'nw': 10,
-    'de-nw': 10, 'nrw': 10, 'nordrhein': 10, 'westfalen': 10, 'nordrhein-westfalen': 10, 'rp': 11, 'de-rp': 11,
+    'de-nw': 10, 'nrw': 10, "nordrhein-westfalen (nrw)": 10, 'nordrhein': 10, 'westfalen': 10,
+    'nordrhein-westfalen': 10, 'rp': 11, 'de-rp': 11, "europäisches parlament": 17,
     'rheinland-pfalz': 11, 'rheinland': 11, 'pfalz': 11, 'sl': 12, 'de-sl': 12, 'saarland': 12, 'saar': 12,
     'sn': 13, 'de-sn': 13, 'sachsen': 13, 'sac': 13, 'st': 14, 'de-st': 14, 'sachsen-anhalt': 14,
     'anhalt': 14, 'sh': 15, 'de-sh': 15, 'schleswig-holstein': 15, 'schleswig': 15, "deutschland": 0,
@@ -106,38 +107,67 @@ def umfrage_ausgeben(parlacode, count: int):
 
 
 class Dawum(commands.Cog):
+    class DropdownButton(discord.ui.Select):
+        def __init__(self, count):
+            super().__init__(placeholder="Wähle ein Parlament aus!", min_values=1, max_values=1, options=[
+                discord.SelectOption(label='Bundestag', description=' Bundestag'),
+                discord.SelectOption(label='Baden-Württemberg',
+                                     description=' Baden-Württemberg'),
+                discord.SelectOption(label='Bayern', description=' Bayern'),
+                discord.SelectOption(label='Berlin', description=' Berlin'),
+                discord.SelectOption(label='Brandenburg',
+                                     description=' Brandenburg'),
+                discord.SelectOption(label='Bremen', description=' Bremen'),
+                discord.SelectOption(label='Hamburg', description=' Hamburg'),
+                discord.SelectOption(label='Hessen', description=' Hessen'),
+                discord.SelectOption(label='Mecklenburg-Vorpommern',
+                                     description=' Mecklenburg-Vorpommern'),
+                discord.SelectOption(label='Niedersachsen',
+                                     description=' Niedersachsen'),
+                discord.SelectOption(label='Nordrhein-Westfalen (NRW)',
+                                     description=' Nordrhein-Westfalen (NRW)'),
+                discord.SelectOption(label='Rheinland-Pfalz',
+                                     description=' Rheinland-Pfalz'),
+                discord.SelectOption(label='Saarland', description=' Saarland'),
+                discord.SelectOption(label='Sachsen', description=' Sachsen'),
+                discord.SelectOption(label='Sachsen-Anhalt',
+                                     description=' Sachsen-Anhalt'),
+                discord.SelectOption(label='Schleswig-Holstein',
+                                     description=' Schleswig-Holstein'),
+                discord.SelectOption(label='Thüringen', description=' Thüringen'),
+                discord.SelectOption(label='Europäisches Parlament',
+                                     description=' Europäisches Parlament')])
+            self.count = count
+
+        async def callback(self, interaction: discord.Interaction):
+            await interaction.response.send_message(embed=umfrage_ausgeben(self.values[0].lower(), count=self.count))
+            await interaction.channel.send(file=discord.File("data/output.png"))
+
+    class DropdownUI(discord.ui.View):
+        def __init__(self, count):
+            super().__init__()
+            self.add_item(Dawum.DropdownButton(count))
 
     def __init__(self, client):
         self.client = client
         self.lock = asyncio.Lock()
 
     @commands.command(aliases=["umfrage"])
-    async def poll(self, ctx, parla="bt", count=1):
+    async def poll(self, ctx, count=1):
         """Gebe die aktuelle Wahlumfrage aus.
-        Syntax: !poll <ländercode> <Umfragenzahl>
-        Der Ländercode ist optional. Alle Ländercodes sind intuitiv. Umfragenzahl"""
+        Syntax: !poll<Umfragenzahl>
+        Wähle anschließend ein Parlament aus!"""
         # The lock ensures that no plotting will be overwritten.
         async with self.lock:
-            if parla.lower() == "help":
-                wahlhelfembed = discord.Embed(
-                    description=
-                    "Verwendung: !poll oder !poll <ländercode> <range>. Länderkürzel der deutschen Bundesländer,"
-                    " Bundestag oder EU Range beliebig wählbar (<= 10) und es wird ein gemittelter Durchschnitt "
-                    "berechnet",
-                    title="Hilfe zum Befehl !poll", color=12370112)
-                await ctx.send(embed=wahlhelfembed, delete_after=self.client.del_time_mid)
-            else:
-                if count < 1:
-                    await ctx.send(embed=discord.Embed(
-                        title="Error!", description="Count muss >= 1 sein"))
-                    return
-                result = umfrage_ausgeben(parla, int(count))
-                if result is None:
-                    await ctx.send(embed=discord.Embed(
-                        title="Error!", description="Dein Parlament konnte nicht gefunden werden!"))
-                else:
-                    await ctx.send(embed=result)
-                    await ctx.send(file=discord.File("data/output.png"))
+            if count < 1:
+                await ctx.send(embed=discord.Embed(
+                    title="Error!", description="Count muss >= 1 sein"))
+                return
+            await ctx.send("Wähle ein Bundesland aus!", view=Dawum.DropdownUI(count))
+
+    @commands.command()
+    async def newpoll(self, ctx):
+        await ctx.send("Wähle ein Bundesland aus!", view=Dawum.DropdownUI())
 
 
 def setup(client):
